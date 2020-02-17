b2008a912cdad493a766ecedf72b68012fb10940
krishnaveni77 <61136773+krishnaveni77@users.noreply.github.com>
2020-02-17 14:10:56 +0530

    hello project

---
+++ java/src/java/Demo.java
@@ -0,0 +1,24 @@
+package java;
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter ur name");
+		String name = sc.nextLine();
+		System.out.println("your Name is "+name);
+		
+		System.out.println("print salary");
+		int salary = sc.nextInt();
+		System.out.println("salary+50000");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
